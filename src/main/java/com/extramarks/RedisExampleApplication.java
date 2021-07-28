f1abb18225fe07151c69d9e3a775d2038af707c3
yesankur <ankur.jvm@gmail.com>
2021-07-28 17:38:09 +0530

    ank-redis-em

---
+++ src/main/java/com/extramarks/RedisExampleApplication.java
@@ -0,0 +1,12 @@
+package com.extramarks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisExampleApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(RedisExampleApplication.class, args);
+    }
+}
---
+++ src/main/java/com/extramarks/queue/MessagePublisher.java
@@ -0,0 +1,6 @@
+package com.extramarks.queue;
+
+public interface MessagePublisher {
+
+    void publish(final String message);
+}
